47cc926d723f906fd324a6d1cf25ae57b7e0393f
Pau Riera <pau.riera.forteza@gmail.com>
2024-09-30 19:06:08 +0200

    Fix exception when no artist was found. Bump to 0.4.12

--- src/lastfmcollagegenerator/exceptions.py
+++ src/lastfmcollagegenerator/exceptions.py
@@ -1,3 +1,7 @@
 class ArtistNotFound(Exception):
     def __init__(self, msg="Artist not found", *args, **kwargs):
         super().__init__(msg, *args, **kwargs)
+
+class ArtistImageNotFound(Exception):
+    def __init__(self, msg="Artist image not found", *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
